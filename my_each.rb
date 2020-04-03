5bff7ffa974149c3809a60dfd084e730f65f4a76
Sandie Nunez <snunez@vt.edu>
2020-04-03 14:54:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(x)
+    while x >= 0 
+    puts x
+    x -= 1 
+  end
 end
+
+def my_method do 
+   
+  my code goes in here
+  
+end
+ #def my_each(words) do |item|
+    #expect(item).to equal(words[counter])
+    #counter+=1
+    #end
+#end
