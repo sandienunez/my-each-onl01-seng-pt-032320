99129b5c322e7418061d818dd1418672256e3b5c
Sandie Nunez <snunez@vt.edu>
2020-04-03 22:05:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,9 @@
 def my_each(x)
     while x >= 0 
-    puts x
     x -= 1 
   end
 end
 
-def my_method do 
-   
-  my code goes in here
-  
-end
  #def my_each(words) do |item|
     #expect(item).to equal(words[counter])
     #counter+=1
